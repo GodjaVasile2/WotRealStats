c512d8c4d0e3571ac612d887521164363994be15
GodjaVasile2 <vasile.godja88@gmail.com>
2023-11-07 11:38:17 +0200

    adding clan and player

--- app.py
+++ app.py
@@ -1,20 +1,61 @@
 import requests
 import config
+import sqlite3
+import time
+from pathlib import Path
 
 params = {
     "type": "exact",
-    "search": "Call_me_Vosi"
+    "search": "N-AGE"
 }
 
-response = requests.get(config.id_url, params=params)
-result = response.json()["data"][0]["account_id"]
+response = requests.get(config.clan_url, params=params)
+if response.status_code == 200:
+    clan_id = response.json()["data"][0]["clan_id"]
 
-# print(result)
+    params = {
+        "clan_id": clan_id
+    }
 
-params = {
-    "account_id": result
-}
+    response = requests.get(config.info_url, params=params)
 
-stats = requests.get(config.stats_url, params=params)
+    if response.status_code == 200:
+        clans_data = response.json()
 
-print(stats.text)
+        clan_details = clans_data["data"][str(clan_id)]
+        clan = (
+            clan_details["clan_id"],
+            clan_details["name"],
+            clan_details["created_at"],
+            clan_details["creator_id"],
+            clan_details["leader_id"],
+            clan_details["color"],
+            clan_details["members_count"]
+        )
+        clan_members = clans_data["data"][str(clan_id)]["members"]
+
+        members_list = []
+        for member in clan_members:
+            player_details = (
+                member["account_id"],
+                member["account_name"],
+                member["role"],
+                member["joined_at"]
+            )
+            members_list.append(player_details)
+    else:
+        print("Error occurred:", response.status_code)
+
+else:
+    print("Error occurred:", response.status_code)
+
+
+with sqlite3.connect("db.sqlite3") as conn:
+    command = "INSERT INTO Clans VALUES(?,?,?,?,?,?,?)"
+
+    conn.execute(command, clan)
+    conn.commit()
+
+
+# current_timestamp = int(time.time())
+# formatted_date = time.strftime("%d/%m/%Y", time.localtime(current_timestamp))
